497d9f070321b0b87cf44d989a70dce00c3136d4
Bill bilguun <bilguun.ts@infinite.mn>
2022-05-24 13:45:06 +0800

    init

---
+++ target/debug/deps/server-764aedf0bc3f9a23.d
@@ -0,0 +1,5 @@
+d:\DevelopmentENV\Rust\Rust light projects\chat\server\target\debug\deps\server-764aedf0bc3f9a23.rmeta: src\main.rs
+
+d:\DevelopmentENV\Rust\Rust light projects\chat\server\target\debug\deps\server-764aedf0bc3f9a23.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/server-9d33571c0ad6fb83.d
@@ -0,0 +1,5 @@
+d:\DevelopmentENV\Rust\Rust light projects\chat\server\target\debug\deps\server-9d33571c0ad6fb83.rmeta: src\main.rs
+
+d:\DevelopmentENV\Rust\Rust light projects\chat\server\target\debug\deps\server-9d33571c0ad6fb83.d: src\main.rs
+
+src\main.rs:
